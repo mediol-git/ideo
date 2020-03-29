fc60fcce072e1cb279ecfbd6592199fa032d1844
mediol-git <mediol26@gmail.com>
2020-03-29 13:18:22 +0300

    fixed switch light/dark

--- js/switch.js
+++ js/switch.js
@@ -1,18 +1,5 @@
 $(document).ready(function(){
     $(".checkbox").click(function(){
-        $(".light").toggleClass("dark"); return false;
+        $(".light").toggleClass("dark");
     });
 });
-
-// Смена изображений
-function chg(id,chk){
-	var el = document.getElementById(id);
-	var ch = document.getElementById(chk);
-	if (el.src.indexOf("1.jpg")>0){
-		el.src="3.jpg"
-		ch.checked="true";
-	}else{
-		el.src="1.jpg"
-		ch.checked="";
-	}
-}
