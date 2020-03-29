76581b90fad3b8916f10868fd7f3447086208386
mediol-git <mediol26@gmail.com>
2020-03-29 15:32:42 +0300

    fixed switch light/dark images

--- js/switch.js
+++ js/switch.js
@@ -1,5 +1,52 @@
-$(document).ready(function(){
+const images = [
+    {
+      selector: '#menuIco',
+      dark: 'img/menu-dark.png',
+      light: 'img/menu.png'
+    },
+    {
+        selector: '#menuIco2',
+        dark: 'img/menu-dark.png',
+        light: 'img/menu.png'
+    },
+    {
+      selector: '#plus-1',
+      dark: 'img/add-dark.png',
+      light: 'img/add.png'
+    },
+    {
+      selector: '#plus-2',
+      dark: 'img/add-dark.png',
+      light: 'img/add.png'
+    },
+    {
+      selector: '#plus-3',
+      dark: 'img/add-dark.png',
+      light: 'img/add.png'
+    },
+    {
+      selector: '#close',
+      dark: 'img/close-dark.png',
+      light: 'img/close.png'
+    },
+    {
+      selector: '#close2',
+      dark: 'img/close-dark.png',
+      light: 'img/close.png'
+    }
+];
+
+function swapImages() {
+    const isDark = $('.light').hasClass('dark');
+    for (let image of images) {
+      $(image.selector).attr('src', isDark ? image.dark : image.light);
+    }
+}
+
+$(document).ready(function () {
     $(".checkbox").click(function(){
         $(".light").toggleClass("dark");
+        swapImages();
     });
+    swapImages();
 });
