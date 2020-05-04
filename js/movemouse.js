44af2032a3a75bd448d0f2197e79b18a1245bc18
mediol-git <mediol26@gmail.com>
2020-05-04 15:42:16 +0300

    dded mouse effect

--- js/movemouse.js
+++ js/movemouse.js
@@ -1,4 +1,4 @@
-const boxes = document.querySelectorAll('.box');
+const boxes = document.querySelectorAll('.img-wrapper');
 const items = [];
 
 const Mode = Object.freeze({
@@ -27,7 +27,7 @@ const update = () => {
 						item.mode = Mode.Return;
 					}
 				} else {
-					[item.velocity.x, item.velocity.y] = [item.position.x / -10, item.position.y / -10];
+					[item.velocity.x, item.velocity.y] = [item.position.x / -50, item.position.y / -50];
 
 					if (Math.abs(item.velocity.x) < 0.1 && Math.abs(item.velocity.y) < 0.1) {
 						[item.position.x, item.position.y] = [0, 0];
@@ -72,8 +72,8 @@ const init = () => {
 			[item.mouse.current.x, item.mouse.current.y] = [event.offsetX, event.offsetY];
 
 			if (item.mouse.previous.x !== null && item.mouse.previous.y !== null) {
-				item.velocity.x += (item.mouse.current.x - item.mouse.previous.x) / 75;
-				item.velocity.y += (item.mouse.current.y - item.mouse.previous.y) / 75;
+				item.velocity.x += (item.mouse.current.x - item.mouse.previous.x) / 250;
+				item.velocity.y += (item.mouse.current.y - item.mouse.previous.y) / 250;
 			}
 			
 			[item.mouse.previous.x, item.mouse.previous.y] = [item.mouse.current.x, item.mouse.current.y];
